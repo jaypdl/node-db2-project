ab93ef712ddb9be2a3d1b79f9861f4ea79e30653
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-16 16:45:39 -0600

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,81 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model')
+const vinValidator = require('vin-validator')
+
+const checkCarId = async(req, res, next) => {
+  try {
+    const idExists = await Car.getById(req.params.car_id)
+    if (!idExists) {
+      res.status(404).json({ message: `car with id ${req.params.car_id} is not found` })
+    } else {
+      req.carInfo = idExists
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+ /*  try {
+    const { vin, make, model, mileage } = req.body
+    const valuesToCheck = [vin, make, model, mileage]
+    valuesToCheck.forEach(value => {
+      if (!value) {
+        res.status(400).json({ message: `${Object.keys({value}[0])} is missing` })
+        return
+      }
+    })
+  } catch (err) {
+    next(err)
+  } */
+  try {
+    const { vin, make, model, mileage } = req.body
+    if (!vin) {
+      res.status(400).json({ message: "vin is missing" })
+    } else if (!make) {
+      res.status(400).json({ message: "make is missing" })
+    } else if (!model) {
+      res.status(400).json({ message: "model is missing" })
+    } else if (!mileage) {
+      res.status(400).json({ message: "mileage is missing" })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next (err)
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    const isValidVin = vinValidator.validate(req.body.vin)
+    if (!isValidVin) {
+      res.status(400).json({ message: `vin ${req.body.vin} is invalid` })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const allCars = await Car.getAll()
+    const found = allCars.find(car => car.vin === req.body.vin) 
+    if (found) {
+      res.status(400).json({ message: `vin ${req.body.vin} already exists` })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (car_id) => {
+  return db('cars').where('car_id', car_id).first()
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (newCar) => {
+  const newId = await db('cars').insert(newCar)
+  return getById(newId)
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
